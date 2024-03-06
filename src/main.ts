8ea99ad2654d668a3c1ff33907de733ff92d34ee
Prajapatirp <prajapatirp.7284@gmail.com>
2024-03-07 02:24:01 +0530

    Added Structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // This line of code for the payload validation.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // This line of code for the swagger builder.
+  const config = new DocumentBuilder()
+    .setTitle('Digiting-media')
+    .setDescription('Digiting Media Project')
+    .setVersion('1.0')
+    .addTag('Digiting-media')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+
+  // This line of code for create swagger document.
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  const port = process.env.APP_PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
+
 bootstrap();
